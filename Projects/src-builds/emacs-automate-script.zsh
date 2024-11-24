a2a657331d70f3d9b334142bc7d8ecdeb771e0cb
Aritro Sen <aretrosen@proton.me>
2024-11-25 02:01:33 +0530

    feat: amongst other things, added aria2

--- Projects/src-builds/emacs-automate-script.zsh
+++ Projects/src-builds/emacs-automate-script.zsh
@@ -6,8 +6,6 @@ if ! sudo -v; then
   exit 1
 fi
 
-# sudo apt install libgnutls28-dev libmagickwand-dev 
-
 pushd emacs-bleeding
 make extraclean
 popd
@@ -18,7 +16,7 @@ git pull --recurse-submodules --all
 popd
 
 pushd emacs-bleeding
-../emacs/configure --with-mailutils --with-sound=alsa --with-pdumper=yes --with-imagemagick --with-tree-sitter --with-pgtk --with-native-compilation=aot --without-compress-install CXXFLAGS="-O2 -g3 -mtune=native -march=native -Wall -Wextra -Wformat=2 -Wtrampolines -Wbidi-chars=any -Werror=format-security -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion -fhardened -fstrict-flex-arrays=3 -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -fexceptions -Wl,-z,nodlopen -Wl,-z,noexecstack" CFLAGS="-O2 -g3 -mtune=native -march=native -Wall -Wextra -Wformat=2 -Wtrampolines -Wbidi-chars=any -Werror=format-security -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion -fhardened -fstrict-flex-arrays=3 -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -fexceptions -Wl,-z,nodlopen -Wl,-z,noexecstack" TREE_SITTER_LIBS="-L/usr/local/lib -ltree-sitter" TREE_SITTER_CFLAGS="-isystem /usr/local/include"
+../emacs/configure --with-mailutils --with-sound=alsa --with-pdumper=yes --with-imagemagick --with-tree-sitter --with-pgtk --with-native-compilation=aot --without-compress-install --with-small-ja-dic CFLAGS="-O2 -g3 -mtune=native -march=native -Wall -Wextra -Wformat=2 -Wtrampolines -Wbidi-chars=any -Werror=format-security -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion -fhardened -fstrict-flex-arrays=3 -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -fexceptions -Wl,-z,nodlopen -Wl,-z,noexecstack" TREE_SITTER_LIBS="-L/usr/local/lib -ltree-sitter" TREE_SITTER_CFLAGS="-isystem /usr/local/include"
 
 make -j$(nproc)
 sudo make -j$(nproc) install
